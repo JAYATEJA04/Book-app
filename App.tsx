a815a3df9f86e949ee65886a8fcb247d3de03c7c
Jaya <akulajayateja4444@gmail.com>
2023-10-30 11:19:13 +0530

    installed firebase and authentication

--- App.tsx
+++ App.tsx
@@ -16,7 +16,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import CombinedScreens from './source/screens/Combined screen/CombinedScreens';
 import LoginScreen from './source/screens/Login Screen/LoginScreen';
 import UserRegistration from './source/screens/Login Screen/RegistrationScreen';
-
+import { ReactNativeFirebase } from '@react-native-firebase/app';
+import auth from '@react-native-firebase/auth';
 const App = () => {
   return (
     <NavigationContainer>
