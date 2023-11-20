6c69d23f255220d77559c554da54ec0db283e400
Jaya <akulajayateja4444@gmail.com>
2023-11-20 17:26:45 +0530

    we can add text and it aligns properly in the UI and button is enabled/disabled on the basis of text entered

--- source/Components/CreatePostScreen.tsx
+++ source/Components/CreatePostScreen.tsx
@@ -16,33 +16,50 @@ const oneFourthWindowHeight = windowHeight / 4;
 const effectiveWidth = windowWidth - 2 * 20;
 
 const CreateTale = () => {
+  const [textPost, setTextPost] = useState('');
+  const isButtonDisabled = textPost.length === 0;
+
   return (
     <View style={styles.container}>
       <View style={styles.container2}>
         {/* post button view */}
         <View style={styles.postButtonView}>
-          <TouchableOpacity style={styles.postButton}>
-            <Text style={styles.postButtonText}>Post</Text>
+          <TouchableOpacity
+            style={
+              isButtonDisabled
+                ? styles.postButtonDisabled
+                : styles.postButtonEnabled
+            }
+            disabled={isButtonDisabled}>
+            <Text
+              style={
+                isButtonDisabled
+                  ? styles.postButtonTextDisabled
+                  : styles.postButtonTextEnabled
+              }>
+              Post
+            </Text>
           </TouchableOpacity>
         </View>
         {/* create post layout */}
         <View style={styles.createLayout}>
-          <Image
-            source={require('../../assets/Photos/me.jpg')}
-            style={{height: 50, width: 50, borderRadius: 50}}
-          />
+          <View style={styles.imageView}>
+            <Image
+              source={require('../../assets/Photos/me.jpg')}
+              style={{height: 50, width: 50, borderRadius: 50}}
+            />
+          </View>
           <View style={styles.textLayout}>
             <TextInput
-              style={styles.textStyle} // Set the text color to green
-              placeholder="Enter text"
-              placeholderTextColor={'black'}
+              style={styles.taleText}
+              placeholder={"What's your tale?"}
+              multiline
+              value={textPost}
+              onChangeText={txt => setTextPost(txt)}
             />
           </View>
         </View>
         {/* border buttom */}
-        <View style={{justifyContent: 'center', alignItems: 'center'}}>
-          <View style={styles.borderBottom} />
-        </View>
       </View>
     </View>
   );
@@ -51,26 +68,29 @@ const CreateTale = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 10,
     backgroundColor: 'white',
   },
   container2: {
-    padding: 10,
+    flex: 1,
     // borderWidth: 1,
   },
   createLayout: {
-    paddingVertical: 10,
-    paddingHorizontal: 10,
     flexDirection: 'row',
     // borderWidth: 1,
-    height: oneFourthWindowHeight,
+  },
+  imageView: {
+    // padding: 1,
+    // borderWidth: 1,
   },
   textLayout: {
-    paddingHorizontal: 10,
+    flex: 1,
+    left: 10,
+    // borderWidth: 1,
   },
-  textStyle: {
+  taleText: {
+    // flexWrap: 'wrap',
     fontSize: 18,
-    color: 'black',
-    flexWrap: 'wrap',
   },
   borderBottom: {
     height: 1,
@@ -83,7 +103,15 @@ const styles = StyleSheet.create({
     height: oneFourthWindowHeight / 5,
     alignItems: 'flex-end',
   },
-  postButton: {
+  postButtonDisabled: {
+    borderRadius: 30,
+    backgroundColor: '#A9A9A9',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: oneFourthWindowHeight / 5,
+    width: 70,
+  },
+  postButtonEnabled: {
     borderRadius: 30,
     backgroundColor: '#39A7FF',
     justifyContent: 'center',
@@ -91,7 +119,12 @@ const styles = StyleSheet.create({
     height: oneFourthWindowHeight / 5,
     width: 70,
   },
-  postButtonText: {
+  postButtonTextDisabled: {
+    color: 'grey',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  postButtonTextEnabled: {
     color: 'white',
     fontSize: 15,
     fontWeight: 'bold',
